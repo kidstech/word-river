c7688747a669b5fd93bb131f5c30472352cf3e10
Richard Lussier <lussi036@morris.umn.edu>
2021-03-27 17:37:16 -0500

    Bring context pack e2e tests from team climate code
    This commit brings the e2e tests for the context pack display and adding a context pack over from team climate's code. Slight modifications were made in order to fit in with the way wordlists work. Epic #3
    
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>
    Co-authored-by:  Elena Lam <lam00071@morris.umn.edu>

---
+++ client/cypress/integration/display-contextPacks.spec.ts
@@ -0,0 +1,46 @@
+import { DisplayContextPacksComponent } from '../support/display-contextPacks.po';
+
+const page = new DisplayContextPacksComponent();
+
+describe('Display Context Pack', () => {
+
+    before(() => {
+        cy.task('seed:database');
+    });
+
+    beforeEach(() => {
+        page.navigateTo();
+    });
+
+    it('Should have 4 context packs', () => {
+        page.getCpCards().should('have.length', 4);
+    });
+
+    it('Should click "View Context Pack" on a context pack and lead to a valid URL', () => {
+        page.getCpCards().first().then((card) => {
+            page.clickViewCp(page.getCpCards().first());
+
+            cy.url().should('match', /\/packs\/[0-9a-fA-F]{24}/);
+
+        });
+    });
+
+    it('Should click "View Context Pack" on a context pack and have one word list', () => {
+      page.getCpCards().first().then((card) => {
+          const cpName = card.find('.cp-card-name').text();
+          const cpEnabled = card.find('.cp-card-enabled').text();
+
+          page.clickViewCp(page.getCpCards().first());
+
+          cy.url().should('match', /\/packs\/[0-9a-fA-F]{24}/);
+
+         page.getWordListCards().should('have.length', 1);
+      });
+    });
+
+    it('Should click add context pack and go to the right URL',() => {
+        page.addCpButton().click();
+        cy.url().should(url => expect(url.endsWith('/packs/new')).to.be.true);
+        cy.get('.add-cp-title').should('have.text', 'New Context Pack');
+    });
+});
