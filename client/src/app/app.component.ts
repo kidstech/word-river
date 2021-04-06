6f97f7cef9ec6ad25993fe0d5c1e2918fc9bd036
Biruk <mengi024@morris.umn.edu>
2021-04-06 02:52:20 -0500

    Add a context pack indicator on navbar and corrected back button
    There is now a context pack indicator in the navbar that tells you which context pack you are in. In addition to this, the back button now appropriately takes you to the right page.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { ContextPackService } from './services/contextPack-service/contextpack.service';
+import { ContextPack } from './datatypes/contextPacks';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +12,46 @@ import {Location} from '@angular/common';
 export class AppComponent implements OnInit {
   title = 'WordRiver';
   backButtonVisible = false;
-  constructor(private location: Location)
-  {}
+  contextPackVisible = false;
+  contextPack: ContextPack;
+
+  constructor(private location: Location, private router: Router, private service: ContextPackService) { }
+
   ngOnInit(): void {
-    this.location.onUrlChange((url,state)=>{
+    this.location.onUrlChange((url, state) => {
       this.backButtonVisible = url !== '/';
+      const parts = url.split('/');
       console.log(url);
+
+      if (parts.length === 3 && parts[1] === 'packs' || parts.length <= 1 || url.length <= 1) {
+        this.contextPackVisible = false;
+        this.contextPack = undefined;
+      } else if (parts.length >= 3 && parts[2]) {
+        this.service.getPack(parts[2]).subscribe(pack => {
+          this.contextPack = pack;
+          this.contextPackVisible = true;
+        });
+      }
+      else {
+        this.contextPackVisible = false;
+      }
     });
   }
 
-  goBack(){
-    this.location.back();
-    // TODO: use location.path() to decide which page to go to.
+  goBack() {
+    const path = this.location.path();
+    if ((/^\/?packs\/new\/?$/g).test(path) || // New Context Pack
+      (/^\/?packs\/.{24}\/?$/g).test(path)) { // Display wordlists
+      this.router.navigate(['']);
+    }
+    else if (
+      (/^\/?packs\/.{24}\/import$/g).test(path) || // Import wordlist
+      (/^\/?packs\/.{24}\/new$/g).test(path) ||  // add wordlist
+      (/^\/?packs\/.{24}\/.{1,100}$/g).test(path) // view wordlist
+    ) {
+      this.router.navigate(['packs/' + path.split('/')[2]]);
+    } else { }
+  }
+  getPage() {
   }
 }
