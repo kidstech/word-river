1d0d7b26a2d35435c265051de22423a3685ac2f0
Biruk <mengi024@morris.umn.edu>
2021-04-17 16:56:12 -0500

    Add validation for login and sign up user input
    We have implemented a login validation however it has some issues that need to be fixed.
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by:  Kyle Day <day00090@morris.umn.edu>
    Co-authored-by:  Jacob Jenness <jenne077@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>
    Co-authored-by:  Elena Lam <lam00071@morris.umn.edu>
    Co-authored-by:  Biruk Mengistu <mengi024@morris.umn.edu>

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
 import { LoginService } from './../../services/login-service/login.service';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -15,50 +17,101 @@ export class LoginComponent implements OnInit {
   signUpPass: string;
   signUpEmail: string;
 
-  validLoginEmail: boolean;
-  validLoginPassword: boolean;
-  validSignUpName: boolean;
-  validSignUpEmail: boolean;
-  validSignUpPassword: boolean;
+  loginForm: FormGroup;
+  signUpForm: FormGroup;
 
-  constructor(private login: LoginService, private router: Router) { }
+  emailPattern = /.{1,50}@.{1,}\..{1,10}\w+/g;
+  // eslint-disable-next-line max-len
+  emailPattern2 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+
+
+  validationMessages = {
+    name: [
+      { type: 'required', message: 'A name is required' },
+      { type: 'maxLength', message: 'Your name must be less than 50 characters'}
+    ],
+
+    email: [
+      { type: 'pattern', message: 'Invalid email entered' }
+    ],
+
+    password: [
+      { type: 'minLength', message: 'Your password must be at least 8 characters long'}
+    ]
+  };
+
+
+
+  constructor(
+    private login: LoginService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
     console.log(this.login.user);
+    this.createForms();
+    if (this.login.isLoggedIn) {
+      this.router.navigate(['packs']);
+    };
+  }
+  update() {
+    console.log(this.loginForm.get('email').hasError('pattern'));
+  }
+  createForms() {
+    //Validates login
+    this.loginForm = this.fb.group({
+      email: new FormControl('', Validators.compose([
+        // eslint-disable-next-line max-len
+        Validators.pattern(this.emailPattern2)
+      ])),
 
-      if (this.login.isLoggedIn) {
-        this.router.navigate(['packs']);
-      };
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(8),
+      ]))
+    });
+
+    //Validates sign up
+    this.signUpForm = this.fb.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.maxLength(50),
+      ])),
+
+      email: new FormControl('', Validators.compose([
+        // eslint-disable-next-line max-len
+        Validators.pattern(this.emailPattern2)
+      ])),
+
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(8),
+      ]))
+    });
   }
-  update(){
-    this.validLoginEmail =
-    this.validateEmail(this.loginEmail);
-    this.validSignUpEmail =
-    this.validateEmail(this.signUpEmail);
-    this.validLoginPassword = this.loginPass.length > 6;
-    this.validSignUpPassword = this.signUpPass.length > 6;
-    console.log(this.loginEmail);
-    console.log(`this.validLoginEmail ${this.validLoginEmail}\nthis.validLoginPass${this.validLoginPassword}`);
-  }
-  signIn(){
+  signIn() {
     this.login.signIn(this.loginEmail, this.loginPass, (uid) => {
       console.log(uid);
       this.router.navigate(['packs']);
     }, (err) => {
-      console.log(err);
+      this.snackBar.open(err, null, {
+        duration: 2000,
+      });
     });
   }
-  signUp(){
-    this.login.signUp('random',this.signUpEmail, this.signUpPass, (uid) => {
+  signUp() {
+    this.login.signUp('random', this.signUpEmail, this.signUpPass, (uid) => {
       console.log(uid);
       this.router.navigate(['packs']);
     }, (err) => {
-      console.log(err);
+      this.snackBar.open(err, null, {
+        duration: 2000,
+      });
     });
   }
   validateEmail(email) {
     // eslint-disable-next-line max-len
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-}
+  }
 }
