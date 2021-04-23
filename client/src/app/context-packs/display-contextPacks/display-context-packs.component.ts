e53c95cb62b082f0657d66cd5b6204fe3bc96159
Biruk <mengi024@morris.umn.edu>
2021-04-23 14:54:06 -0500

    Modify display context packs to fit new aesthetic
    Modified the look of the displaycontextpacks component to adhere to the new look.

--- client/src/app/context-packs/display-contextPacks/display-context-packs.component.ts
+++ client/src/app/context-packs/display-contextPacks/display-context-packs.component.ts
@@ -7,6 +7,7 @@ import { ContextPackService } from '../../services/contextPack-service/contextpa
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { safe } from 'src/testing/utils';
+import { Learner } from 'src/app/datatypes/learner';
 
 @Component({
   selector: 'app-display-context-packs',
@@ -20,26 +21,41 @@ export class DisplayContextPacksComponent implements OnInit, OnDestroy {
   public icon: string;
   public enabled: boolean;
   public wordlist: Array<WordList>;
-  public learners = [];
+  public learners: Learner[] = [];
   getPackSub: Subscription;
 
+  verbCount;
+  adjectiveCount;
+  miscCount;
+  nounCount;
+
+  assignedCount;
+  unAssignedCount;
+
+  enabledCount;
+  disabledCount;
+
+
   constructor(private packService: ContextPackService, private router: Router,
-     private login: LoginService,
-     private users: UserService
-     ) { }
+    private login: LoginService,
+    private users: UserService
+  ) { }
 
   getPacksFromServer(): void {
     this.unsub();
     this.getPackSub = this.packService.getUserPacks(this.login.user.authId).subscribe(
       returnedPacks => {
         this.contextPacks = returnedPacks;
+        this.users.getLearners(this.login.authID).subscribe(learners => {
+          this.learners = learners;
+          this.initValues();
+          this.count();
+        });
       }, err => {
         console.log(err);
       });
     safe(() => { this.name = this.login.user.name; }, this.login.user);
-    this.users.getLearners(this.login.authID).subscribe(learners=>{
-      this.learners = learners;
-    });
+
   }
 
   ngOnInit(): void {
@@ -59,8 +75,46 @@ export class DisplayContextPacksComponent implements OnInit, OnDestroy {
   removeCP(id: string) {
     this.packService.deleteContextPackFromAll(this.login.user.authId, id).subscribe(() => {
       this.contextPacks = this.contextPacks.filter(cp => cp._id !== id);
+      this.initValues();
+      this.count();
     });
     return id;
+  }
+
+  initValues(){
+    this.verbCount = 0;
+    this.adjectiveCount = 0;
+    this.miscCount = 0;
+    this.nounCount = 0;
+    this.assignedCount = 0;
+    this.unAssignedCount = 0;
+    this.enabledCount = 0;
+    this.disabledCount = 0;
+  }
+
+  count() {
+    this.contextPacks.forEach(cp => cp.wordlists.forEach(wl => {
+      this.nounCount += wl.nouns.length;
+      this.verbCount += wl.verbs.length;
+      this.adjectiveCount += wl.adjectives.length;
+      this.miscCount += wl.misc.length;
+    }));
+    this.contextPacks.forEach(cp => {
+      if (this.learners.some(learner =>
+        learner.learnerPacks.some(id => {
+          console.log(`cp.id = ${cp._id} and learnerPackId = ${id} so ${cp._id === id}`);
+          return cp._id === id;
+        }))) {
+        this.assignedCount++;
+      } else {
+        this.unAssignedCount++;
+      }
+      if(cp.enabled){
+        this.enabledCount++;
+      } else{
+        this.disabledCount++;
+      }
+    });
   }
 
 }
