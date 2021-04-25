d52843a3c24dc11e9c5647e8ffe9e286774ad798
Joshua Eklund <eklun124@umn.edu>
2021-04-25 17:21:49 -0500

    Create Cypress tests for editing a learner
    This commit creates cypress tests for the feature of editing a learner. Issue #42
    
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by:  Kyle Day <day00090@morris.umn.edu>
    Co-authored-by:  Jacob Jenness <jenne077@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>
    Co-authored-by:  Elena Lam <lam00071@morris.umn.edu>
    Co-authored-by:  Biruk Mengistu <mengi024@morris.umn.edu>

---
+++ client/cypress/integration/editLearner.spec.ts
@@ -0,0 +1,69 @@
+import { EditLearnerPage } from 'cypress/support/edit-learner.po';
+import { LoginPage } from 'cypress/support/login.po';
+
+
+const page = new EditLearnerPage();
+const loginPage = new LoginPage();
+describe('Edit Learner', () => {
+
+  before(() => {
+    cy.task('seed:database');
+  });
+
+  beforeEach(()=> {
+    loginPage.navigateTo();
+    loginPage.login();
+    cy.wait(1000);
+  });
+
+  it('Should assign and unassign a context pack', () => {
+    page.getLearnerCards().first().then((card) => {
+      page.clickLearner(page.getLearnerCards().first());
+    });
+
+      cy.url().should('match', /\/learners\/[0-9]+/);
+
+      page.getAssignedPacks().should('have.length', 1);
+
+      page.getCpCards().first().then((cpCard)=> {
+        page.unassignPackButton().click();
+      });
+
+      page.getAssignedPacks().should('have.length', 0);
+
+      page.saveLearnerButton().click();
+      page.getAssignedPacksCount().eq(1).should('have.text', '0Packs');
+
+      page.getLearnerCards().eq(1).then((card) => {
+        page.clickLearner(page.getLearnerCards().eq(1));
+      });
+
+      cy.url().should('match', /\/learners\/[0-9]+/);
+
+      page.getAssignedPacks().should('have.length', 0);
+      page.getCpCards().first().then((cpCard)=> {
+
+      page.assignPackButton().eq(0).click();
+        });
+      page.getAssignedPacks().should('have.length', 1);
+      page.saveLearnerButton().click();
+      page.getAssignedPacksCount().eq(1).should('have.text', '1Packs');
+  });
+
+  it('Should change the name of the learner', () => {
+    page.getLearnerCards().eq(1).then((card) => {
+      page.clickLearner(page.getLearnerCards().eq(1));
+    });
+
+      cy.url().should('match', /\/learners\/[0-9]+/);
+
+    page.getNameField().clear();
+    page.getNameField().type('Go Tigers');
+
+    page.saveLearnerButton().click();
+
+    page.getLearnerCards().eq(1).then((card) => {
+      page.getLearnerCardName().eq(1).should('have.text', 'Go Tigers');
+    });
+  });
+});
