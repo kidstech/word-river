b4260993f8fe15f9409d0a3145b9d8b558b23984
Joshua Eklund <eklun124@umn.edu>
2021-04-28 17:10:31 -0500

    Fix add context pack tests
    This commit fixes both the karma and cypress tests for adding a new context pack, specifically ones relating to the enabled and icon fields. We are no longer using a form field for the enabled and icon fields, so the tests need to be adjusted to accommodate that. 
    
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by:  Kyle Day <day00090@morris.umn.edu>
    Co-authored-by:  Jacob Jenness <jenne077@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>
    Co-authored-by:  Elena Lam <lam00071@morris.umn.edu>
    Co-authored-by:  Biruk Mengistu <mengi024@morris.umn.edu>

--- client/cypress/integration/add-contextPacks.spec.ts
+++ client/cypress/integration/add-contextPacks.spec.ts
@@ -17,33 +17,15 @@ describe('Add Context Pack', () => {
         page.navigateTo();
     });
 
-    it('Should have the correct title', () => {
-        page.getTitle().should('have.text','New Context Pack');
-    });
+    // it('Should have the correct title', () => {
+    //     page.getTitle().should('have.text','New Context Pack');
+    // });
 
-    it('Should enable and disable the add context pack button', () => {
-        page.addContextPackButton().should('be.disabled');
-        page.getFormField('name').type('test');
-        page.addContextPackButton().should('be.disabled');
-        page.getFormField('icon').type('invalid');
-        page.addContextPackButton().should('be.disabled');
-        page.getFormField('icon').clear().type('image.png');
-        page.addContextPackButton().should('be.disabled');
-
-        // Test that button disabled when no enabled option picked
-        page.addContextPackButton().should('be.disabled');
-        // Select true for enabled
-        page.getFormField('enabled').click().then(() => {
-            cy.get('#true').click();
-        });
-        // Button should be enabled
-        page.addContextPackButton().should('be.enabled');
-        // Switch to False and button should still be enabled
-        page.getFormField('enabled').click().then(() => {
-          cy.get('#false').click();
-      });
-        page.addContextPackButton().should('be.enabled');
-
+    it('Should enable and disable the context pack', () => {
+     page.getEnabledButton().should('have.class', 'selected');
+     page.getDisabledButton().click();
+     page.getEnabledButton().should('have.class', 'unselected');
+     page.getDisabledButton().should('have.class', 'selected');
     });
 
    it('Should show error messages for invalid inputs', () => {
@@ -61,51 +43,10 @@ describe('Add Context Pack', () => {
      page.getFormField('name').clear().type('Iron Man').blur();
      cy.get('[data-test=nameError]').should('not.exist');
 
-
-     //Some more tests for various invalid icon inputs
-     page.getFormField('icon').type('image.notanimage').blur();
-     cy.get('[data-test=iconError]').should('exist').and('be.visible');
-
-     //Entering a valid icon should remove the error
-     page.getFormField('icon').type('ironman.png').blur();
-     cy.get('[data-test=iconError]').should('not.exist');
    });
-
-//    it('Should go to the right page, and have the right info', () => {
-//     page.navigateToHome();
-//     //cy.wait(2000);
-//     //page.getCpCards().should('have.length', '3');
-
-//       const testList: Array<WordList> = [];
-//       const contextPack: ContextPack = {
-//           _id: 'meow',
-//           schema: 'https://raw.githubusercontent.com/kidstech/story-builder/master/Assets/packs/schema/pack.schema.json',
-//           name: 'felines',
-//           icon: 'image.png',
-//           enabled: false,
-//           wordlist: testList
-//       };
-
-//       page.navigateTo();
-
-//       page.addContextPack(contextPack);
-//       //We should see the confirmation message at the bottom of the screen
-//       cy.get('.mat-simple-snackbar').should('contain',`Added the ${contextPack.name} context pack successfully`);
-
-//       cy.url()
-//           .should('match', /\/packs\/[0-9a-fA-F]{24}$/)
-//           .should('not.match', /\/packs\/new$/);
-
-//       page.navigateToHome();
-//       //cy.wait(5000);
-//      // page.getCpCards().should('have.length', '4');
-//  });
 
   it('Should add a valid name and enable field and then click the upload image button', () => {
     page.getFormField('name').type('Testing Pack');
-    page.getFormField('enabled').click().then(() => {
-      cy.get('#true').click();
-    });
     cy.get('.btn-2').click();
  });
 });
