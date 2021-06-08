6a0372cd3f7dd8836b50c3ca7c98ae36e9d65306
K.K. Lamberty <lamberty@morris.umn.edu>
2021-06-08 14:24:59 -0500

    Update Firebase API keys
    Update Firebase API keys and credentials to a new Firebase instance instead of the one from the semester project.
    
    Co-authored-by: Elena Lam <lam00071@morris.umn.edu>
    Co-authored-by: Ben Burgess <burge337@morris.umn.edu>

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,12 +6,13 @@ export const environment = {
   production: false,
   apiUrl: '/api/',
   firebaseConfig: {
-    apiKey: 'AIzaSyB3P9RFZSBHx0gWYyHzFAcvGDiJs7oWUHY',
-    authDomain: 'wordriver-57fe8.firebaseapp.com',
-    projectId: 'wordriver-57fe8',
-    storageBucket: 'wordriver-57fe8.appspot.com',
-    messagingSenderId: '371696233375',
-    appId: '1:371696233375:web:5a7c3b772add5e79ad6baa'
+    apiKey: 'AIzaSyAQiKwre6qGNQK0Pcz_XKEyy-e9w-x-6s0',
+    authDomain: 'word-river.firebaseapp.com',
+    projectId: 'word-river',
+    storageBucket: 'word-river.appspot.com',
+    messagingSenderId: '631848148168',
+    appId: '1:631848148168:web:b62d9de24e398e8d1159fd',
+    measurementId: 'G-9XRY12QV3R'
   }
 };
 
