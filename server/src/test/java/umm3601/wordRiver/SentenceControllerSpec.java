ba8a69179487e41471cf9a5f0e9043ae23bdd854
Joshua Eklund <eklun124@umn.edu>
2022-02-20 22:52:20 -0600

    Finish tests for SentenceController
    This commit finishes the tests for the SentenceController.java file. This commit writes the tests for the postStory method found in the controller.

--- server/src/test/java/umm3601/wordRiver/SentenceControllerSpec.java
+++ server/src/test/java/umm3601/wordRiver/SentenceControllerSpec.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.mockrunner.mock.web.MockHttpServletRequest;
@@ -16,15 +18,17 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
+import org.eclipse.jetty.util.ajax.JSON;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.javalin.http.util.ContextUtil;
 import io.javalin.plugin.json.JavalinJson;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 
@@ -99,5 +103,329 @@ public class SentenceControllerSpec {
       Context ctx = ContextUtil.init(mockReq, mockRes, "api/sentences/:learnerId", ImmutableMap.of("learnerId", learnerId));
 
       assertThrows(NotFoundResponse.class, () -> {sentenceController.getSentences(ctx);});
+    }
+
+
+    @Test
+    public void postValidSentence() {
+      //String jsonString = "{'sentenceId':'2f1d0e3d-1660-44af-9bea-cc4deccf546d','sentenceText':'Go Tigers','timeSubmitted':'2/20/2022 8:53:12 PM','learnerId':'1623445120497','words': [{";
+      String testStory = "{" +
+        "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+          "\"sentenceText\": \"Go Tigers\"," +
+          "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+          "\"learnerId\": \"1623445120499\"," +
+          "\"words\": [" +
+            "{" +
+              "\"word\": \"Go\"," +
+              "\"forms\": [" +
+                "\"Go\"" +
+              "]" +
+            "}," +
+            "{" +
+              "\"word\": \"Tigers\"," +
+              "\"forms\": [" +
+                "\"Tiger\"," +
+                "\"Tigers\"" +
+              "]" +
+            "}" +
+          "]," +
+          "\"selectedWordForms\": [" +
+            "\"Go\","+
+            "\"Tigers\"" +
+          "]," +
+          "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+          "\"contextPackIds\": [" +
+            "\"610376992c96b6238f61bc1a\"," +
+            "\"60beabcbbbaa96763c50ae16\"" +
+          "]" +
+        "}";
+
+        System.out.println(testStory);
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(testStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+      sentenceController.postSentence(ctx);
+
+      assertEquals(201, mockRes.getStatus());
+
+      Document newSentence = db.getCollection("sentences").find(Filters.eq("learnerId", testId)).first();
+
+      assertEquals("Go Tigers", newSentence.get("sentenceText"));
+    }
+
+    @Test
+    public void postInvalidSentence() {
+      String invalidStory = "banana";
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postInvalidSentenceNoSentenceId() {
+      String invalidStory = "{" +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+
+    }
+
+    @Test
+    public void postStoryNoLearnerId() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postStoryNoSentenceText() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postStoryNoTimeSubmitted() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postStoryNoWords() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postStoryNoSelectedWordForms() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"userId\": \"60c1a985926f9c1edcc3a6e8\"," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
+    }
+
+    @Test
+    public void postStoryNoUserId() {
+      String invalidStory = "{" +
+      "\"sentenceId\": \"2f1d0e3d-1660-44af-9bea-cc4deccf546d\"," +
+        "\"sentenceText\": \"Go Tigers\"," +
+        "\"timeSubmitted\": \"2/20/2022 8:53:12 PM\"," +
+        "\"learnerId\": \"1623445120499\"," +
+        "\"words\": [" +
+          "{" +
+            "\"word\": \"Go\"," +
+            "\"forms\": [" +
+              "\"Go\"" +
+            "]" +
+          "}," +
+          "{" +
+            "\"word\": \"Tigers\"," +
+            "\"forms\": [" +
+              "\"Tiger\"," +
+              "\"Tigers\"" +
+            "]" +
+          "}" +
+        "]," +
+        "\"selectedWordForms\": [" +
+          "\"Go\","+
+          "\"Tigers\"" +
+        "]," +
+        "\"contextPackIds\": [" +
+          "\"610376992c96b6238f61bc1a\"," +
+          "\"60beabcbbbaa96763c50ae16\"" +
+        "]" +
+      "}";
+
+      String testId = "1623445120499";
+      mockReq.setBodyContent(invalidStory);
+      mockReq.setMethod("POST");
+      Context ctx = ContextUtil.init(mockReq, mockRes, "/api/sentences/:learnerId", ImmutableMap.of("learnerId", testId));
+
+      assertThrows(BadRequestResponse.class, () -> {sentenceController.postSentence(ctx);});
     }
 }
