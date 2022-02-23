1cb494311f1f3e18e84afdd27ba8e5bc13509d31
Joshua Eklund <eklun124@umn.edu>
2022-02-22 22:21:24 -0600

    Create HTML table that displays learner word counts
    This commit creates an HTML table in the learner data page that displays the words learners have seen and how many times they have seen them.

--- client/src/app/learners/learner-data/learner-data.component.ts
+++ client/src/app/learners/learner-data/learner-data.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LearnerData } from 'src/app/datatypes/learnerData';
+import { LearnerDataService } from 'src/app/services/learnerData-service/learner-data.service';
+
 
 @Component({
   selector: 'app-learner-data',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learner-data.component.scss']
 })
 export class LearnerDataComponent implements OnInit {
+  learnerData: LearnerData;
+  learnerName: string;
+  learnerId: string;
+  learnerWords: Map<string, number>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private learnerDataService: LearnerDataService
+  ) { }
+
 
   ngOnInit(): void {
-  }
-
+    this.route.paramMap.subscribe((pmap) => {
+      this.learnerId = pmap.get('id');
+      this.learnerDataService.getLearnerData(this.learnerId).subscribe(res=> {
+        this.learnerData = res;
+        this.learnerWords = res.wordCounts;
+        this.learnerName = res.learnerName;
+      });});
+    }
 }
