f8f750baa4aaf567fb5f0cc818c814647abed059
Joshua Eklund <eklun124@umn.edu>
2022-03-02 18:00:57 -0600

    Get paginators for tables to work
    This commit makes the paginators for the word count table and sentence table working.
    
    Co-authored-by: Natasha-Zebrev  <zebre001@morris.umn.edu>

--- client/src/app/learners/learner-data/learner-data.component.ts
+++ client/src/app/learners/learner-data/learner-data.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LearnerData } from 'src/app/datatypes/learnerData';
 import { WordCounts } from 'src/app/datatypes/wordCounts';
 import { Sentence } from 'src/app/datatypes/sentence';
 import { LearnerDataService } from 'src/app/services/learnerData-service/learner-data.service';
 import { SentencesService } from 'src/app/services/sentences-service/sentences.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 
 @Component({
@@ -12,7 +15,11 @@ import { SentencesService } from 'src/app/services/sentences-service/sentences.s
   templateUrl: './learner-data.component.html',
   styleUrls: ['./learner-data.component.scss']
 })
-export class LearnerDataComponent implements OnInit {
+export class LearnerDataComponent implements OnInit, AfterViewInit {
+  @ViewChild('wordCountPaginator') wordCountPaginator: MatPaginator;
+  @ViewChild('sentencePaginator') sentencePaginator: MatPaginator;
+  sentenceDataSource = new MatTableDataSource<Sentence>();
+  wordCountDataSource = new MatTableDataSource<WordCounts>();
   learnerData: LearnerData;
   learnerName: string;
   learnerId: string;
@@ -27,6 +34,11 @@ export class LearnerDataComponent implements OnInit {
     private sentencesService: SentencesService
   ) { }
 
+  ngAfterViewInit() {
+    this.sentenceDataSource.paginator = this.sentencePaginator;
+    this.wordCountDataSource.paginator = this.wordCountPaginator;
+  }
+
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((pmap) => {
@@ -34,11 +46,11 @@ export class LearnerDataComponent implements OnInit {
 
       this.sentencesService.getSentences(this.learnerId).subscribe(res=> {
         this.sentences = res;
+        this.sentenceDataSource.data = this.sentences;
       });
       this.learnerDataService.getLearnerData(this.learnerId).subscribe(res=> {
         this.learnerData = res;
         this.learnerWords = res.wordCounts;
-        console.log(this.learnerWords);
         this.convertLearnerWordsMapToArray();
         this.learnerName = res.learnerName;
       });});
@@ -52,5 +64,6 @@ export class LearnerDataComponent implements OnInit {
         word.count = value;
         this.wordCountArray.push(word);
       }
+      this.wordCountDataSource.data = this.wordCountArray;
     }
   }
