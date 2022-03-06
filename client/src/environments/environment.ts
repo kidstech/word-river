d18abed6fdd0219176da80d61b2c0e507e8a85db
Joshua Eklund <eklun124@umn.edu>
2022-03-05 21:28:45 -0600

    Fix more ng-lint errors
    Change double quotes to single quotes

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   apiUrl: '/api/',
   firebaseConfig: {
-    apiKey: "AIzaSyA0HZ6qa_AebqRV-s6atpgUDN11M6GAaSk",
-    authDomain: "word-river-26472.firebaseapp.com",
-    projectId: "word-river-26472",
-    storageBucket: "word-river-26472.appspot.com",
-    messagingSenderId: "414776996526",
-    appId: "1:414776996526:web:d94bad8ad66b8df163b33f",
-    measurementId: "G-6CVMLD46XT"
+    apiKey: 'AIzaSyA0HZ6qa_AebqRV-s6atpgUDN11M6GAaSk',
+    authDomain: 'word-river-26472.firebaseapp.com',
+    projectId: 'word-river-26472',
+    storageBucket: 'word-river-26472.appspot.com',
+    messagingSenderId: '414776996526',
+    appId: '1:414776996526:web:d94bad8ad66b8df163b33f',
+    measurementId: 'G-6CVMLD46XT'
   }
 };
 
