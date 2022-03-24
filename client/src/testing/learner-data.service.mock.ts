0d6d68f62fb729bb0207dae4159cf6f41686eec9
Joshua Eklund <eklun124@umn.edu>
2022-03-24 09:52:11 -0500

    Create mock services for karma testing
    This commit creates mock  learner-data and sentences services to be used in karma testing.

---
+++ client/src/testing/learner-data.service.mock.ts
@@ -0,0 +1,32 @@
+import { Observable, of } from 'rxjs';
+import { LearnerData } from 'src/app/datatypes/learnerData';
+import { LearnerDataService } from 'src/app/services/learnerData-service/learner-data.service';
+
+export class MockLearnerDataService extends LearnerDataService {
+  static wordMap: Map<string, number>;
+  static sessionTimes: Map<string, string>;
+
+  static testLearnerData: LearnerData = {
+    learnerId: '1623445108911',
+    learnerName: 'Bob',
+    wordCounts: MockLearnerDataService.wordMap,
+    sessionTimes: MockLearnerDataService.sessionTimes
+  };
+
+  constructor() {
+    super(null);
+  }
+
+  getLearnerData(id: string): Observable<LearnerData> {
+    MockLearnerDataService.wordMap.set('mango', 1);
+    MockLearnerDataService.wordMap.set('tangerine', 1);
+    MockLearnerDataService.wordMap.set('apple', 3);
+    MockLearnerDataService.wordMap.set('banana', 5);
+    MockLearnerDataService.wordMap.set('orange', 1);
+    MockLearnerDataService.wordMap.set('grape', 1);
+    MockLearnerDataService.sessionTimes.set('3/8/2022 8:27:50 PM', '');
+
+    return of(MockLearnerDataService.testLearnerData);
+  }
+
+}
