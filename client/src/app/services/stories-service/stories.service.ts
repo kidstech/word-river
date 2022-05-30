5e9ddca0fad275f16ad2906c172e2eefb7777cf9
Joshua Eklund <eklun124@umn.edu>
2022-05-30 15:17:58 -0500

    Create story datatypes and services
    This commit creates the story datatypes and services for the client side aspect of viewing learner stories.
    
    Co-authored-by: Natasha-Zebrev <zebre001@morris.umn.edu>

---
+++ client/src/app/services/stories-service/stories.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Story } from 'src/app/datatypes/story';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoriesService {
+  readonly storiesUrl: string = environment.apiUrl + 'stories';
+  userUrl: string;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getLearnerStories(learnerId: string): Observable<Story[]> {
+    return this.httpClient.get<Story[]>(this.userUrl + '/' + learnerId);
+  }
+}
