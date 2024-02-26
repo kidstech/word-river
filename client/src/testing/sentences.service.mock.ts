d2fcb64c34c5e8d63f89f56bba06f1b8b0c1c0ec
Brendan Conroy <conro188@morris.umn.edu>
2024-02-26 01:20:55 -0600

    Replace new stuff with old mock sentence service

--- client/src/testing/sentences.service.mock.ts
+++ client/src/testing/sentences.service.mock.ts
@@ -1,35 +1,34 @@
-import { Sentence } from 'src/app/datatypes/sentence';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { LearnerData } from 'src/app/datatypes/learnerData';
+import { LearnerDataService } from 'src/app/services/learnerData-service/learner-data.service';
 
-export const mockSentence: Sentence = {
-  sentenceId: '1',
-  sentenceText: 'This is sample sample sentence.',
-  timeSubmitted: '2022-01-01T12:00:00',
-  learnerId: 'learner1',
-  words: [{ word: 'sample', forms: ['sample'] }],
-  selectedWordForms: [],
-  userId: 'user1',
-  contextPackIds: ['contextPack1'],
-  wordCountPairs: [],
-  repeatedWords: [{word: 'sample', count: 2 }],
-  uniqueWords: [{word: 'word', count: 1 }],
-  uniqueWordCount: 0,
-  wordCountMap: {},
+@Injectable()
+export class MockLearnerDataService extends LearnerDataService {
+  static wordMap = new Map<string, number>();
+  static sessionTimes = new Map<string, string>();
 
-  // calculateRepeatedWordsWithCount: function (sentenceText: string) {
-  //   const wordCountMap = new Map<string, number>();
-  //   const words = sentenceText.toLowerCase().split(/\s+/);
+  static testLearnerData: LearnerData = {
+    learnerId: '1623445108911',
+    learnerName: 'Bob',
+    wordCounts: MockLearnerDataService.wordMap,
+    sessionTimes: MockLearnerDataService.sessionTimes
+  };
 
-  //   // Count occurrences of each word
-  //   words.forEach((word) => {
-  //     const currentCount = wordCountMap.get(word) || 0;
-  //     wordCountMap.set(word, currentCount + 1);
-  //   });
+  constructor() {
+    super(null);
+  }
 
-  //   // Create an array of objects with word and count properties
-  //   const repeatedWords = Array.from(wordCountMap.entries())
-  //     .filter(([word, count]) => count > 1)
-  //     .map(([word, count]) => ({ word, count }));
+  getLearnerData(id: string): Observable<LearnerData> {
+    MockLearnerDataService.wordMap.set('mango', 1);
+    MockLearnerDataService.wordMap.set('tangerine', 1);
+    MockLearnerDataService.wordMap.set('apple', 3);
+    MockLearnerDataService.wordMap.set('banana', 5);
+    MockLearnerDataService.wordMap.set('orange', 1);
+    MockLearnerDataService.wordMap.set('grape', 1);
+    MockLearnerDataService.sessionTimes.set('3/8/2022 8:27:50 PM', '');
 
-  //   return repeatedWords;
-  // },
-};
+    return of(MockLearnerDataService.testLearnerData);
+  }
+
+}
